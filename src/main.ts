a64dc723c8e54af4a0b77d2984a614b1bff266fc
hoanjen <lew2k3@gmail.com>
2024-05-25 15:19:55 +0700

    fix-required-2

--- src/main.ts
+++ src/main.ts
@@ -27,6 +27,7 @@ async function bootstrap() {
     'http://localhost:3000',
     'http://localhost:3001',
     'http://localhost:8000',
+    'http://localhost:8000/',
     'https://mmomarket.onrender.com',
     'https://mmomarket.onrender.com/api',
   ];
