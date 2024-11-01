f91d7eb4d51446a1ba19562692d81cb83eecdbda
hoanjen <lew2k3@gmail.com>
2024-11-01 16:41:37 +0700

    update connect socket

--- src/modules/gateway/gateway.service.ts
+++ src/modules/gateway/gateway.service.ts
@@ -10,19 +10,41 @@ export class GatewayService {
     this.redisClient.connect();
   }
 
-  async getSocketId(user_id: string) {
-    return this.redisClient.hGet('user_socket_ids', user_id);
+  async addSocketId(user_id: string, socket_id: string) {
+    await this.redisClient.sAdd(`user:${user_id}:socket_ids`, socket_id);
   }
 
-  async setSocketId(user_id: string, socket_id: string) {
-    return this.redisClient.hSet('user_socket_ids', user_id, socket_id);
+  async getSocketIds(user_id: string): Promise<string[]> {
+    return await this.redisClient.sMembers(`user:${user_id}:socket_ids`);
   }
 
-  async delSocketId(user_id: string) {
-    return this.redisClient.hDel('user_socket_ids', user_id);
+  async removeSocketId(user_id: string, socket_id: string) {
+    await this.redisClient.sRem(`user:${user_id}:socket_ids`, socket_id);
   }
 
-  async getSocketIds(user_ids: string[]) {
-    return this.redisClient.hmGet('user_socket_ids', user_ids);
+  async removeAllSocketIds(user_id: string) {
+    await this.redisClient.del(`user:${user_id}:socket_ids`);
+  }
+  async getAllSocketIds(user_ids: string[]): Promise<string[]> {
+    const pipeline = this.redisClient.multi();
+
+    user_ids.forEach((user_id) => {
+      pipeline.sMembers(`user:${user_id}:socket_ids`);
+    });
+
+    const results = await pipeline.exec();
+
+    const allSocketIds: string[] = [];
+    results.forEach((result) => {
+      if (result[1]) {
+        allSocketIds.push(...result[1]);
+      }
+    });
+
+    return allSocketIds;
+  }
+
+  async onModuleDestroy() {
+    await this.redisClient.disconnect();
   }
 }
